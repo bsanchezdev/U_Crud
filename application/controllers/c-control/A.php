a902089af6fe9e84566c82c70560c64c8f062b73
bsanchez <bsanchezcardenas@gmail.com>
2015-11-26 15:31:05 -0300

    First

---
+++ application/controllers/c-control/A.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of A
+ *
+ * @author Desarrollo
+ */
+class A extends CI_Controller{
+    public function __construct() {
+        parent::__construct();
+        
+        $this->load->library("u_crud");
+                $this->u_crud
+                     ->init("default");
+    }
+    
+    public function index() {
+        $this->pag();
+    }
+    
+    public function pag() {
+        
+        /*$this->u_crud
+           ->use_db("ripley_sitrel")
+           ->tabla("banco_ripley_carga")
+           ->show();*/
+     //    $configx['base_url']       = base_url()."fuckyeah"  ; // parametro base de la aplicación, si tenemos un .htaccess nos evitamos el index.php
+	// $configx['total_rows']     = 12        ;//calcula el número de filas  
+     //    $configx['per_page']       = 6               ; 
+        $config["uri_segment"]      =3;
+        $config['base_url']         = base_url()."c-control/A/pag";
+        $this->u_crud
+           ->use_db("default")
+           ->query("select RUT,DV,NOMBRE from banco_ripley_carga")
+          ->set_config_pag($config)
+           ->show();
+        }
+    
+}
---
+++ application/views/U_crud/show_tabla.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?= $title;?> - Paginación Codeigniter 3</title>
+     <?= load_bootstrap_css() ?>
+     <?= load_bootstrap_js() ?>
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.10/css/jquery.dataTables.css">
+  
+<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.10/js/jquery.dataTables.js"></script>
+</head>
+<body>
+    
+    <div class="container">
+       
+    <div class="row">
+        <div class="col-xs-12 table-responsive">
+    <table id="<?= $id_tab ?>" class="table table-striped table-condensed display">
+    <thead>
+    <?php
+    if($data_tabla)
+    {
+    ?>
+    <tr>
+    <?php foreach ($columnas as $key => $value) { ?>
+    <th> <?= $value; ?> </th>     
+    <?php } ?>     
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($data_tabla as $k => $v){ $count++?>
+    <tr id="<?php echo $count ;?>">
+    <?php foreach ($v as $key => $value) {  ?>
+    <td><?= $value ?></td>
+    <?php } ?>
+    </tr>
+    <?php
+    }
+    
+    }
+    ?>
+    </tbody>
+    </table>
+        </div>
+    </div>
+    <?= $this->pagination->create_links() ?>
+    </div>
+    <script>
+        $(document).ready( function () {
+    var selected = [];        
+            
+            
+    $('#<?= $id_tab?>9').DataTable(
+            {
+    paging: false,
+    searching: true,
+    ordering:  true,
+    select: true, 
+    info:     true,
+    "columnDefs": [
+            {
+                "targets": [ 3 ],
+                "visible": false,
+                "searchable": false
+            },
+            {
+                "targets": [ 4 ],
+                "visible": false
+            },
+            {
+                "targets": [ 5 ],
+                "visible": false
+            },
+            {
+                "targets": [ 6 ],
+                "visible": false
+            }
+        ],
+    //"processing": true,
+    //    "serverSide": true,
+    //    "ajax": "scripts/ids-arrays.php",
+        "rowCallback": function( row, data ) {
+            if ( $.inArray(data.DT_RowId, selected) !== -1 ) {
+                $(row).addClass('selected');
+            }
+        }
+}
+            );
+    
+    
+    
+     $('#<?= $id_tab?> tbody').on('click', 'tr', function () {
+        var id = this.id;
+        var index = $.inArray(id, selected);
+ 
+        if ( index === -1 ) {
+            selected.push( id );
+        } else {
+            selected.splice( index, 1 );
+        }
+ 
+        $(this).toggleClass('selected');
+       // alert(id);
+    } );
+} );
+    </script>
+</body>
+</html>
